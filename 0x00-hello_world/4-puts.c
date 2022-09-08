c92e7be51ee1b8240f90c7b8d7e032a2930a2b1f
MachariaS <samwelkaranja322@gmail.com>
2022-09-08 14:29:31 -0700

    First commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ *
+ *  * main - entry point
+ *
+ *   * Return: 0
+ *
+ *    */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
