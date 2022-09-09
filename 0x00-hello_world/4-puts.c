23d67d4d0a23d51606bd242c3a6a5a8c3d3e441c
MachariaS <samwelkaranja322@gmail.com>
2022-09-09 11:29:01 -0700

    Shell basics

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - entry point
- *
+ *  * main - main block
  *   * Return: 0
- *
  *    */
-
 int main(void)
-
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
