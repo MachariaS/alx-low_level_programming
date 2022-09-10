70f83b07406f1488db863374dc64a4d29466499d
MachariaS <samwelkaranja322@gmail.com>
2022-09-10 06:24:03 -0700

    hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,10 @@
  *
  * main - main block
  * Description: 'use puts function to print'
- * Return: 0
+ * Return: 0 (suceess)
  *
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
