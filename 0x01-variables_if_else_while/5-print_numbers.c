ddcc1fb6457c8627ef859259b7405b9cf2c3179d
MachariaS <samwelkaranja322@gmail.com>
2022-09-11 10:41:27 -0700

    fisrt scommit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Description: print single digit numbers of base 10 starting from 0,\n
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
