cc32633fd566c7d99839ec2a926fa6f99bfae9ab
MachariaS <samwelkaranja322@gmail.com>
2022-09-11 12:45:04 -0700

    gh

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: program that prints all possible combinations
+ * return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+		if (n != 9)
+		{
+			putchar(',');
+			putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
