aa9418b31e83dd055f21bd16c11cf1be7852f808
MachariaS <samwelkaranja322@gmail.com>
2022-09-12 14:07:45 -0700

    p

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,16 +8,17 @@
 
 int main(void)
 {
-	int n;
+	int n = '0';
 
-	for (n = 0; n <= 9;)
+	while (n <= '9')
 	{
 		putchar(n);
-		if (n != 9)
+		if (n != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
+		++n;
 	}
 	putchar('\n');
 	return (0);
