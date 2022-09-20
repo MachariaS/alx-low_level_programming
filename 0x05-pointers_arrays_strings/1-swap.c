3b8c03f2f63153a79ee87197cfc2d15214ce09a3
MachariaS <samwelkaranja322@gmail.com>
2022-09-20 11:53:53 -0700

    correction

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * swap_int - function that swaps the values of two integers.
- *@a: interger one
- @b: interger two
+ * swap_int - function that swaps the values of two integers
+ * @a: interger one
+ * @b: interger two
  * Return: void
  */
 
