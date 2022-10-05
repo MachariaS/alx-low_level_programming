d97538d33b819b9e8e64d8614a314675c88ab1fd
MachariaS <99346489+MachariaS@users.noreply.github.com>
2022-10-05 22:57:45 +0300

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- *  * File: main.h
- *   * Auth: Alagwu David
- *    * Desc: Header file containing the prototypes of all functions
- *     *       included in the static library libholberton.a.
- *      */
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
